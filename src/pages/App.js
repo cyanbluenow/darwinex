b79e26f2791424d9be7c1e4b57edc0bf7e93cd69
unknown <jluiscanton@gmail.com>
2021-10-20 09:00:19 +0200

    firt commit

---
+++ src/pages/App.js
@@ -0,0 +1,25 @@
+import logo from "../resources/logo darwinex.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://darwinex.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Darwinex Homepage
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
