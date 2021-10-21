7e4091a6b2921049ec095bde236e1ad6a2dad7e3
cyanbluenow <jluiscanton@gmail.com>
2021-10-21 16:49:20 +0200

    card carousel menu without functions

--- src/components/CardsCarousel.js
+++ src/components/CardsCarousel.js
@@ -1,6 +1,10 @@
 import data from "./../mockup/data";
 import "./CardsCarousel.scss";
 import TradeCard from "./TradeCard";
+import listIcon from "./../resources/list-icon.svg";
+import cardIcon from "./../resources/card-icon.svg";
+import switchOn from "./../resources/switch.svg";
+import arrowDown from "./../resources/arrow-down.svg"
 import leftArrow from "./../resources/circle-left-arrow-icon.svg";
 import rightArrow from "./../resources/circle-right-arrow-icon.svg";
 
@@ -9,6 +13,26 @@ function CardsCarousel() {
     return (
         <section className="cards-carousel-big-container">
             <section className="cards-carousel-menu-container">
+                <div className="cards-carousel-menu-list">
+                    <img src={listIcon} className="" alt="icon" />
+                </div>
+                <div className="cards-carousel-menu-card">
+                    <img src={cardIcon} className=""  alt="icon"/>
+                </div>
+                <div className="cards-carousel-menu-switch">
+                    <div className="card-carousel-menu-text">
+                        <span>In Portfolio</span>
+                    </div>
+                    <div className="cards-carousel-menu-switch-icon">
+                        <img src={switchOn} className="" alt="switch" />
+                    </div>
+                </div>
+                <div className="cards-carousel-menu-dropdown">
+                    <div className="cards-carousel-menu-text">
+                        <span>Order by Return</span>
+                    </div>
+                    <img src={arrowDown} className="" alt="icon" />
+                </div>
             </section>
             <section className="card-carousel-stats-container">
                 <div className="card-carousel-stats-elements">
